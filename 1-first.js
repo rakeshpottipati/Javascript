18b49e96e3de0c510d2289b10bf314b7c5a0ce0d
rakesh <rakeshpottipati3@gmail.com>
2025-05-18 15:29:43 +0530

    variables

--- 1-first.js
+++ 1-first.js
@@ -1,2 +1,19 @@
-console.log("Hello world");
-console.log(123);
+//console.log("Hello world");
+//document.write(123);
+
+//var age = (10);
+//console.log(age);
+
+
+let age = 40;
+let customerName = "Rakesh";
+
+console.log(age);
+console.log(customerName);
+
+var customerID = 99;
+console.log(customerID);
+
+const num = [1,2,3,4];
+console.log(num);
+
