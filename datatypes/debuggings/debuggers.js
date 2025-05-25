2a788dd8a1d5f7a4dc9b70c1ee3f68708636b3d6
rakesh <rakeshp.fullstack2@gmail.com>
2025-05-25 16:44:04 +0530

    debugging

---
+++ datatypes/debuggings/debuggers.js
@@ -0,0 +1,21 @@
+let foodItemName = "biriyani";
+//console.log(foodItemName);
+document.write(foodItemName);
+debugger;  //stop point
+
+let deliveryLocation = "bangalore city";
+console.log(deliveryLocation);
+document.write(deliveryLocation);
+debugger;  
+
+
+let estimateDeliveryTime = "30 Mins";
+console.log(estimateDeliveryTime);
+//document.write(estimateDeliveryTime);
+debugger;  
+
+
+let isDeliveryAvailable = true;
+console.log(isDeliveryAvailable);
+//document.write(isDeliveryAvailable);
+debugger;  
